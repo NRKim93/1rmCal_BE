226f6c7628f0af41ca1c014e676f12fb9a6384dd
nrk <nr.kim@soliouscorp.com>
2025-12-22 20:10:37 +0900

    fix : cors 구문

--- src/common/security/cors.config.ts
+++ src/common/security/cors.config.ts
@@ -1,8 +1,13 @@
 import {CorsOptions} from "@nestjs/common/interfaces/external/cors-options.interface";
 import * as process from "node:process";
 
+const origins = (process.env.CORS_ORIGIN || 'http://localhost:3000')
+    .split(',')
+    .map(v => v.trim())
+    .filter(Boolean); 
+
 export const corsConfig : CorsOptions = {
-    origin : process.env["CORS_ORIGIN"] || 'http://localhost:3000',
+    origin : origins,
     credentials : true,
     methods : ['GET', "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ['Content-Type', 'Authorization'],
